066d1a151dbae5c00a79ef4d564efdbe7cd73fb7
arminhrbat <92021852+arminhrbat@users.noreply.github.com>
2022-05-22 15:20:29 +0200

    test

---
+++ app/src/main/java/com/example/levoyage/databaseConn.java
@@ -0,0 +1,14 @@
+package com.example.levoyage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class databaseConn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database_conn);
+    }
+}
