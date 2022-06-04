2f5067321326c1b0701c4aa0df66a58a11c00494
Adnan Kuljančić <adnan.kuljancic@outlook.com>
2022-06-04 17:25:17 +0200

    profile screen

--- app/src/main/java/com/example/levoyage/ProfileFragment.java
+++ app/src/main/java/com/example/levoyage/ProfileFragment.java
@@ -1,18 +1,33 @@
 package com.example.levoyage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+        View view = inflater.inflate(R.layout.fragment_profile,container,false);
+        TextView textView = view.findViewById(R.id.editProfileTV);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), EditProfile.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
+
+
 }
