42fb6c08aa9c1f59f352915d21ec2daad1af7c59
Tarik <tarik905.suljic@hotmail.com>
2022-06-09 11:53:13 +0200

    userDatabase uradjena

---
+++ app/src/main/java/com/example/levoyage/User.java
@@ -0,0 +1,95 @@
+package com.example.levoyage;
+
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id; //autoGenerate da generise novi unikatan key
+
+    private String name;
+    private String surname;
+    private String email;
+    private int birthYear;
+    private String username;
+    private String password;
+
+    @Ignore
+    public User(String name, String surname, String email, int birthYear, String username, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.birthYear = birthYear;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(int id, String name, String surname, String email, int birthYear, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.birthYear = birthYear;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/example/levoyage/UserDAO.java
@@ -0,0 +1,37 @@
+package com.example.levoyage;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import java.util.List;
+
+@Dao
+public interface UserDAO {
+
+    @Insert
+    public void addUser(User user);
+
+    @Query("SELECT * FROM users")
+    public List<User> getAllUsers();
+
+    @Query("SELECT * FROM users WHERE id = :user_id LIMIT 1")
+    public User getUser(int user_id);
+
+    @Query("SELECT * FROM users WHERE username = :username LIMIT 1")
+    public User getUserByUsername(String username);
+
+    @Query("SELECT id FROM users WHERE username = :username LIMIT 1")
+    public int getUserIDByUsername(String username);
+
+    @Query("SELECT * FROM users WHERE username = :username AND password = :password LIMIT 1")
+    public User checkIfLoginLegit(String username, String password);
+
+    @Delete
+    public void deleteUser(User user);
+
+    @Update
+    public void updateUser(User user);
+
+}
